9f432db907da75054641057ae4f5ffabacddf3b3
Dennis Law <den.law@uga.edu>
2014-04-07 10:53:07 -0400

    Consolidated game logic into TetrisBoard
    Now you can increment by tick() and get the current board by getBoard()

--- TetrisBoard.java
+++ TetrisBoard.java
@@ -4,12 +4,22 @@ public class TetrisBoard {
 	private TetriminoQueue tetQ;
 	private TetriminoGenerator tetGen;
 	private Tetrimino tet; // Holder for the current paying block.
-	private int[][] playingBoard; //Keeps track of placed blocks. The integer value inside will determine what "color" block was placed there.
+	//Keeps track of placed blocks. The integer value inside will determine what "color" block is there in the second digit.
+	//first digit represents whether the block is in play or not. 0 for "Not in play" and 1 for "In play".
+	private int[][] playingBoard;
 	private boolean gameFinish;
 	private int score;
+	private boolean blockInPlay;
+	private boolean collisionOnce; //Used for checking if a single tick passed and a block is blocked from going down twice.
+	private boolean collisionTwice;
+	private boolean placeBlock;
 	
 	public TetrisBoard(){
 		gameFinish = false;
+		blockInPlay = false;
+		collisionOnce = false;
+		collisionTwice = false;
+		placeBlock = false;
 		score = 0;
 		tetGen = new TetriminoGenerator();
 		
@@ -19,12 +29,30 @@ public class TetrisBoard {
 		
 		//Construct the playingBoard here.
 		
-		playingBoard = new int[10][22];
+		playingBoard = new int[10][24];
 		
 		for(int x = 0; x < 10; x++){
-			for(int y = 0; y < 0; y++){
+			for(int y = 0; y < 24; y++){
 				playingBoard[x][y] = 0;
 			}
+		}
+	}
+	
+	//Ticks every interval. All game logic occurs here.
+	public void tick(){
+		
+		if(!blockInPlay){
+			tet = getBlock();
+			
+			//Handle loading in the block here.
+			loadBlock();
+		}
+		
+		checkCollision();
+		
+		//CollisionOnce is set by checkCollision. If it detects that a static block is underneath a playable block, it flags "CollisionOnce" and prevents the piece from incrementing downwards.
+		if(!collisionOnce){
+			moveDown();
 		}
 	}
 	
@@ -38,17 +66,111 @@ public class TetrisBoard {
 		}
 	}
 	
+	public void checkCollision(){
+		if(collisionOnce){
+			collisionTwice = true;
+			collisionOnce = false;
+		}
+		
+		for(int y = 23; y > -1; y++){
+			for(int x = 0; x < 10; x++){
+				//Checks for blocks in play and then checks if there is space below it.
+				if(playingBoard[x][y]/10 == 1 && playingBoard[x][y+1]%10 != 0 || y == 23){
+					if(collisionTwice){
+						placeBlock = true;
+					}
+					else{
+						collisionOnce = true;
+					}
+				}
+			}
+		}
+		
+		if(placeBlock && collisionTwice){
+			blockPlace();
+			collisionTwice = false;
+			placeBlock = false;
+		}
+		else{
+			collisionTwice = false;
+			placeBlock = false;
+		}
+	}
+	
+	public void moveDown(){
+		for(int y = 23; y > -1; y++){
+			for(int x = 0; x < 10; x++){
+				//Checks for blocks in play and then moves them down one.
+				if(playingBoard[x][y]/10 == 1){
+					playingBoard[x][y+1] = 10 + tet.getType();
+				}
+			}
+		}
+	}
+	
 	//Inspects a block from the queue. To be used for displaying "Blocks to come".
 	public Tetrimino inspectBlock(int e){
 		return tetQ.element(e);
 	}
 	
-	//Takes 4 xy-values and places the blocks into TetrisBlock[][] accordingly. First digit is x-value, second digit is y-value, and third digit is blockValue.
-	public void blockPlace(int xyv1, int xyv2, int xyv3, int xyv4){
-		playingBoard[xyv1/100][(xyv1/10)%10] = xyv1%100;
-		playingBoard[xyv2/100][(xyv2/10)%10] = xyv2%100;
-		playingBoard[xyv3/100][(xyv3/10)%10] = xyv3%100;
-		playingBoard[xyv4/100][(xyv4/10)%10] = xyv4%100;
+	public void loadBlock(){
+		if(tet.getType() == 1){
+			playingBoard[5][0] = 11;
+			playingBoard[5][1] = 11;
+			playingBoard[5][2] = 11;
+			playingBoard[5][3] = 11;
+		}
+		else if(tet.getType() == 2){
+			playingBoard[5][0] = 12;
+			playingBoard[5][1] = 12;
+			playingBoard[5][2] = 12;
+			playingBoard[4][2] = 12;
+		}
+		else if(tet.getType() == 3){
+			playingBoard[4][0] = 13;
+			playingBoard[4][1] = 13;
+			playingBoard[4][2] = 13;
+			playingBoard[5][2] = 13;
+		}
+		else if(tet.getType() == 4){
+			playingBoard[4][0] = 14;
+			playingBoard[5][0] = 14;
+			playingBoard[4][1] = 14;
+			playingBoard[5][1] = 14;
+		}
+		else if(tet.getType() == 5){
+			playingBoard[6][0] = 15;
+			playingBoard[5][0] = 15;
+			playingBoard[5][1] = 15;
+			playingBoard[4][1] = 15;
+		}
+		else if(tet.getType() == 6){
+			playingBoard[4][0] = 16;
+			playingBoard[5][0] = 16;
+			playingBoard[6][0] = 16;
+			playingBoard[5][1] = 16;
+		}
+		else{
+			playingBoard[4][0] = 17;
+			playingBoard[5][0] = 17;
+			playingBoard[5][1] = 17;
+			playingBoard[6][1] = 17;
+		}
+		
+		blockInPlay = true;
+	}
+	
+	//Takes 4 xy-values and places the blocks into TetrisBlock[][] accordingly. First digit is x-value, second digit is y-value, third digit is blockValue.
+	public void blockPlace(){
+		for(int x = 0; x < 10; x++){
+			for(int y = 0; y < 24; y++){
+				if(playingBoard[x][y]/10 == 1){
+					playingBoard[x][y] = tet.getType();
+				}
+			}
+		}
+		
+		blockInPlay = false;
 	}
 	
 	//Returns the block line that's been cleared. Modifies the board as necessary. This should run in a while loop until -1 is achieved.
