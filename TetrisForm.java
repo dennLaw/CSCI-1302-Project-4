2261bb2d274476c3ec763c9be403452cebe78654
panamaklz <klz13@uga.edu>
2014-04-09 13:32:27 -0400

    Fixed the color change in clear line

--- TetrisForm.java
+++ TetrisForm.java
@@ -409,7 +409,7 @@ public class TetrisForm extends javax.swing.JFrame {
     	
     	
     	else if (blockType == 6){
-    		finalBlockType = 2;
+    		finalBlockType = 6;
     		String block = projPath + "\\art\\6.png";
 			
     		if (blockNumber == 1){
@@ -431,7 +431,7 @@ public class TetrisForm extends javax.swing.JFrame {
     	
     	
     	else if (blockType == 7){
-    		finalBlockType = 2;
+    		finalBlockType = 7;
     		String block = projPath + "\\art\\7.png";
 			
     		if (blockNumber == 1){
@@ -508,38 +508,6 @@ public class TetrisForm extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(TetrisForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(TetrisForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(TetrisForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(TetrisForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new TetrisForm().setVisible(true);
-                
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLayeredPane LayeredPane;
